37a7be5c6caa8d046bf835205617bdc1cc498c83
Juha Järvensivu <juha.matti.jarvensivu@gmail.com>
2023-12-03 22:21:59 +0200

    Handle newlines in file reading

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 def read_input(file_path):
     try:
         with open(file_path, 'r', encoding="utf-8") as f:
-            content = f.readlines()
+            content = [line.rstrip() for line in f.readlines()]
         return content
     except FileNotFoundError:
         print(f"Error: File '{file_path}' not found.")
