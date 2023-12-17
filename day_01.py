ca714824d4b62308dc97762797727570725f71a4
Juha Järvensivu <juha.matti.jarvensivu@gmail.com>
2023-12-17 23:29:23 +0200

    Refactor day 1

--- day_01.py
+++ day_01.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from utils import read_input
+from functools import reduce
 
 digit_dictionary = {
     "one": "1",
@@ -14,21 +15,13 @@ digit_dictionary = {
 }
 
 
-def find_digits_in_letters(line):
-    first_digit = last_digit = None
-    for key in digit_dictionary.keys():
-        first_digit_index = line.find(key)
-        last_digit_index = line.rfind(key)
+def update_digits(line, digits):
+    first_results = [(line.find(key), value) for key, value in digit_dictionary.items() if key in line] + [digits[0]]
+    last_results = [(line.rfind(key), value) for key, value in digit_dictionary.items() if key in line] + [digits[1]]
 
-        if first_digit_index != -1:
-            if first_digit is None:
-                first_digit = (first_digit_index, digit_dictionary[key])
-                last_digit = (last_digit_index, digit_dictionary[key])
-            else:
-                if first_digit_index < first_digit[0]:
-                    first_digit = (first_digit_index, digit_dictionary[key])
-                if last_digit_index > last_digit[0]:
-                    last_digit = (last_digit_index, digit_dictionary[key])
+    first_digit = reduce(lambda x, y: x if x[0] < y[0] else y, first_results)
+    last_digit = reduce(lambda x, y: x if x[0] > y[0] else y, last_results)
+
     return [first_digit, last_digit]
 
 
@@ -43,37 +36,27 @@ def find_digits(line):
     return [first_digit, last_digit]
 
 
-def process_line(line, include_digits_in_letters):
+def part_one(line):
     digits = find_digits(line)
-
-    if include_digits_in_letters:
-        digits_in_letters = find_digits_in_letters(line)
-        if digits_in_letters[0] is None or digits_in_letters[1] is None:
-            return int(digits[0][1] + digits[1][1])
-
-        if digits[0] is None or digits_in_letters[0][0] < digits[0][0]:
-            digits[0] = digits_in_letters[0]
-
-        if digits[1] is None or digits_in_letters[1][0] > digits[1][0]:
-            digits[1] = digits_in_letters[1]
-
     return int(digits[0][1] + digits[1][1])
 
 
-def process_file(file_path, include_str_digits):
-    file_content = read_input(file_path)
-    if file_content:
-        return sum(process_line(line, include_str_digits) for line in file_content)
-    return 0
+def part_two(line):
+    digits = find_digits(line)
+    digits = update_digits(line, digits)
+
+    return int(digits[0][1] + digits[1][1])
 
 
 def day_01():
     print("Advent of Code - day 1")
     file_path = Path('.', 'data', 'input_01.txt')
-    solution1 = process_file(file_path, False)
-    print(f"Solution for part one is: {solution1}")
-    solution2 = process_file(file_path, True)
-    print(f"Solution for part two is: {solution2}")
+    file_content = read_input(file_path)
+    if file_content:
+        solution1 = sum(part_one(line) for line in file_content)
+        print(f"Solution for part one: {solution1}")
+        solution2 = sum(part_two(line) for line in file_content)
+        print(f"Solution for part two: {solution2}")
 
 
 day_01()
